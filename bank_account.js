c28e8f17c67867a1026f30bf390e7558b6390ed9
DevialdiMp <devialdimaisaputra03@gmail.com>
2024-09-27 18:43:34 +0700

    mengubah function deposit dan withdraw

--- bank_account.js
+++ bank_account.js
@@ -1,91 +1,52 @@
-let akun = new BankAccount(0);
-
-// function ini untuk memperbarui tampilan saldo
-function updateSaldo() {
-  document.getElementById("TampilanSaldo").innerText = akun.getSaldo();
-}
-
-// function ini untuk tambahsaldo
-function tambahSaldo() {
-  try {
-    let jumlah = parseFloat(
-      window.prompt("Masukkan jumlah saldo yang ingin anda tambahkan:")
-    );
-    if (!isNaN(jumlah) && jumlah > 0) {
-      akun.deposit(jumlah).then(() => {
-        alert(
-          "Saldo anda berhasil ditambahkan. \nSaldo saat ini: Rp. " +
-            akun.getSaldo()
-        );
-        updateSaldo();
-      });
-    } else {
-      alert(
-        "Jumlah yang dimasukkan tidak valid.\nSilahkan masukkan jumlah yang benar!"
-      );
-    }
-  } catch (error) {
-    alert("Terjadi kesalahan: " + error.message);
+class BankAccount {
+  constructor(saldoAwal = 0) {
+    this.saldo = saldoAwal;
+    this.riwayatTransaksi = []; // untuk menyimpan data riwayat transaksi
   }
-}
 
-// function ini untuk mengurangi/penarikan saldo
-function kurangiSaldo() {
-  try {
-    let jumlah = parseFloat(
-      window.prompt("Masukkan jumlah saldo yang ingin anda tarik:")
-    );
-    if (!isNaN(jumlah) && jumlah > 0) {
-      akun
-        .withdraw(jumlah)
-        .then(() => {
-          alert(
-            "Saldo berhasil ditarik. \nSisa saldo anda saat ini: Rp. " +
-              akun.getSaldo()
-          );
-          updateSaldo();
-        })
-        .catch((error) => {
-          alert("Terjadi kesalahan saat menarik saldo: " + error.message);
+  // Function ini berguna untuk mendapatkan saldo
+  getSaldo() {
+    return this.saldo;
+  }
+
+  // Function ini untuk mengambil riwayat transaksi
+  getRiwayatTransaksi() {
+    return this.riwayatTransaksi;
+  }
+
+  // Mensimulasikan operasi deposit secara asynchronous
+  deposit(jumlah) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        this.saldo += jumlah;
+        this.riwayatTransaksi.push({
+          //data yang akan dikirimkan ke riwayat transaksi
+          jenis: "Deposit",
+          jumlah: jumlah,
+          tanggal: new Date().toLocaleString(),
         });
-    } else {
-      alert("Jumlah yang dimasukkan tidak valid.");
-    }
-  } catch (error) {
-    alert("Terjadi kesalahan: " + error.message);
-  }
-}
-
-// function ini untuk menghapus saldo [sebenernya gaguna, tapi saya bikin aja biar lengkap ada tambah, kurang dan hapus hehe:)]
-function deleteSaldo() {
-  try {
-    if (confirm("Apakah Anda yakin ingin menghapus saldo?")) {
-      akun.saldo = 0;
-      alert(
-        "Saldo berhasil dihapus. \nSaldo anda saat ini: Rp. " + akun.getSaldo()
-      );
-      updateSaldo();
-    }
-  } catch (error) {
-    alert("Terjadi kesalahan: " + error.message);
-  }
-}
-
-// function ini untuk melihat riwayat transaksi
-function riwayatSaldo() {
-  try {
-    let riwayat = akun.getRiwayatTransaksi();
-    let riwayatList = document.getElementById("RiwayatTransaksi");
-    riwayatList.innerHTML = ""; // Kosongkan daftar sebelumnya
-
-    riwayat.forEach((transaksi) => {
-      let li = document.createElement("li");
-      li.innerText = `${transaksi.jenis} sebesar Rp. ${transaksi.jumlah} || ${transaksi.tanggal}`;
-      riwayatList.appendChild(li);
+        resolve();
+      }, 3000); // ini menunda deposit/ tambah saldo selama 3 detik
     });
-  } catch (error) {
-    alert("Terjadi kesalahan: " + error.message);
+  }
+
+  // Mensimulasikan operasi withdraw secara asynchronous
+  withdraw(jumlah) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (jumlah <= this.saldo) {
+          this.saldo -= jumlah;
+          this.riwayatTransaksi.push({
+            //data yang akan dikirimkan ke riwayat transaksi
+            jenis: "Withdraw",
+            jumlah: jumlah,
+            tanggal: new Date().toLocaleString(),
+          });
+          resolve();
+        } else {
+          reject(`${jumlah} gagal.`);
+        }
+      }, 4000); // ini menunda withdraw/ penarikan selama 4 detik
+    });
   }
 }
-
-updateSaldo();
